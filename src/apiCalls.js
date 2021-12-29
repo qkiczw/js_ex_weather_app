8dbddf0d6def1f73bda68c078f32830c4d0d38ab
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-12-29 20:21:31 +0100

    function to get forecast by a geolocation works but only if you press the button twice. I aslo added todo and cleaned some unnecessary lines

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,14 +1,19 @@
 import { drawForecastDataInHtml } from "./drawData";
 
+const API_KEYS = {
+  openweather: "dd5bbad88362bfa4029566ec28d36062",
+  geoapify: "adedf4d6425641f5a8e1fbff8da47505",
+};
+
 let forecastData = {
   name: "",
   lat: "",
   lon: "",
 };
 
-export async function getForecastByCityName(searchFieldVal, apiKey) {
+export async function getForecastByCityName(searchFieldVal) {
   await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${searchFieldVal}&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${searchFieldVal}&appid=${API_KEYS.openweather}`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -21,7 +26,7 @@ export async function getForecastByCityName(searchFieldVal, apiKey) {
     .catch((error) => console.log("error: ", error));
 
   await fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${forecastData.lat}&lon=${forecastData.lon}&exclude=minutely&units=metric&lang=pl&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${forecastData.lat}&lon=${forecastData.lon}&exclude=minutely&units=metric&lang=pl&appid=${API_KEYS.openweather}`
   )
     .then((response) => response.json())
     .then((data) => (forecastData.forecast = data))
@@ -30,22 +35,24 @@ export async function getForecastByCityName(searchFieldVal, apiKey) {
   drawForecastDataInHtml(forecastData);
 }
 
-export async function getForecastByGeolocation(apiKey) {
+export function getForecastByGeolocation() {
   navigator.geolocation.getCurrentPosition((succes) => {
     let { latitude, longitude } = succes.coords;
-    console.log(latitude, longitude);
+
     fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&lang=pl&appid=${apiKey}`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&lang=pl&appid=${API_KEYS.openweather}`
     )
       .then((response) => response.json())
-      .then((data) => console.log("forecastdata: ", data))
+      .then((data) => (forecastData.forecast = data))
       .catch((error) => console.log("error", error));
 
     fetch(
-      `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=adedf4d6425641f5a8e1fbff8da47505`
+      `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${API_KEYS.geoapify}`
     )
       .then((response) => response.json())
-      .then((data) => console.log("geoapify: ", data))
+      .then((data) => (forecastData.name = data.results[0].city))
       .catch((error) => console.log("error", error));
   });
+
+  drawForecastDataInHtml(forecastData);
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { getForecastByCityName, getForecastByGeolocation } from "./apiCalls";
 let dayjs = require("dayjs");
 require("dayjs/locale/pl");
 
-const API_KEY = "dd5bbad88362bfa4029566ec28d36062";
 const searchFieldValue = document.querySelector(".search-form__input");
 const searchBtn = document.querySelector(".search-form__btn__search");
 const locationBtn = document.querySelector(".search-form__btn__location");
@@ -16,7 +15,7 @@ searchBtn.addEventListener("click", showForecastByCityName);
 function showForecastByCityName(e) {
   e.preventDefault();
 
-  getForecastByCityName(searchFieldValue.value, API_KEY);
+  getForecastByCityName(searchFieldValue.value);
 }
 
 locationBtn.addEventListener("click", showForecatsByCoords);
@@ -24,24 +23,9 @@ locationBtn.addEventListener("click", showForecatsByCoords);
 function showForecatsByCoords(e) {
   e.preventDefault();
 
-  getForecastByGeolocation(API_KEY);
-
-  // navigator.geolocation.getCurrentPosition((succes) => {
-  //   let { latitude, longitude } = succes.coords;
-  //   console.log("Coords success: ", succes);
-
-  //   fetch(
-  //     `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&lang=pl&appid=${API_KEY}`
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       drawForecastDataInHtml(data);
-  //       console.log("navData", data);
-  //     })
-  //     .catch((error) => console.log("error", error));
-  // });
+  getForecastByGeolocation();
 }
 
-// TODO use localstorage to save last picked latitude and longitude
-// TODO search by city name
+// TODO use localstorage to save last picked latitude and longitude or city name
 // TODO rwd
+// TODO Chart reset function to draw updated data
